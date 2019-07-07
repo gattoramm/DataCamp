9a1badf67d2b1216f45086b6abfe452c4aea566b
RAMIL ARSAEV <gattoramm@gmail.com>
2019-07-07 22:37:17 +0400

    add files

--- Skill/SQL/SQL Fundamentals/4 SQL for Exploratory Data Analysis/1 What's in the database/Read an entity relationship diagram.sql
+++ Skill/SQL/SQL Fundamentals/4 SQL for Exploratory Data Analysis/1 What's in the database/Read an entity relationship diagram.sql
@@ -6,3 +6,15 @@ GROUP BY type
  -- Order the results with the most common
  -- tag types listed first
 ORDER BY count DESC;
+
+-- Select the 3 columns desired
+SELECT name, tag_type.tag, tag_type.type
+FROM company
+  	   -- Join to the tag_company table
+       INNER JOIN tag_company 
+       ON company.id = tag_company.company_id
+       -- Join to the tag_type table
+       INNER JOIN tag_type
+       ON tag_company.tag = tag_type.tag
+  -- Filter to most common type
+WHERE type='cloud';
